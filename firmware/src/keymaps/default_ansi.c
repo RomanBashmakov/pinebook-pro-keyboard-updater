4a12bea82f4726774fb3bc6a0b69c57b6a3da241
Roman Bashmakov <bashmakovra@raitibor.ru>
2026-08-05 18:13:20 +0300

    Fn+f7 шлет w  через массив

--- firmware/src/keymaps/default_ansi.c
+++ firmware/src/keymaps/default_ansi.c
@@ -22,7 +22,7 @@
 #define STARTING_ADDR 0x3000
 
 __code __at (0x0815) uint16_t keymap[] = KEYMAP_ANSI(
-    FR(11), FS(2), FS(3), FS(4), R(F4), FS(0), FS(1), R(F7), FR(1), FR(2), FR(3), FR(4), FR(5),
+    FR(11), FS(2), FS(3), FS(4), R(F4), FS(0), FS(1), FS(5), FR(1), FR(2), FR(3), FR(4), FR(5),
     R(GRV),  R(1), R(2), R(3), R(4), R(5), R(6), FK(7), FK(8), FK(9), FK(10), R(MINS), R(EQL), FR(10) , 
     R(TAB),   R(Q), R(W), R(E), R(R), R(T), R(Y), FK(4), FK(5), FK(6), FK(11), R(LBRC), R(RBRC), R(BSLS),
     R(CAPS),   R(A), R(S), R(D), R(F), R(G), R(H), FK(1), FK(2), FK(3), FK(12), R(QUOT), R(ENT),
@@ -37,7 +37,7 @@ __code uint16_t fns_special[] = {
     [2] = REG_FN(KC_F1, KC__MUTE),
     [3] = REG_FN(KC_F2, KC__VOLDOWN),
     [4] = REG_FN(KC_F3, KC__VOLUP),
-    [5] = REG_FN(KC_F7, 0xFE) // toggle touchpad
+    [5] = REG_FN(KC_F7, KC_W) // ОТЛАДКА: буква W вместо toggle touchpad
 };
 
 // __code __at (0x0941)
